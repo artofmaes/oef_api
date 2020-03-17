7b04b71362b55d434f82886cb7b8af202f053dfc
artofmaes <jordi19@live.be>
2020-03-17 22:08:15 +0100

    Extra Clean-Up

--- api/ApiActions.php
+++ api/ApiActions.php
@@ -11,10 +11,8 @@ class ApiActions
     }
 
     public function addData(){
-        $query = 'INSERT INTO taak SET taa_datum = :taa_datum, taa_omschr = :taa_omschr';
-        $data = json_decode(file_get_contents('php://input'));
-        $stmt = $this->pdo->prepare($query);
 
+        $data = json_decode(file_get_contents('php://input'));
         //zijn de veldjes klaar?
         if (!isset($data->taa_datum) or !isset($data->taa_omschr)) {
             print 'Bruh, vul alles eens in';
@@ -24,9 +22,9 @@ class ApiActions
             $taa_omschr    = htmlentities($data->taa_omschr);
             $taa_datum     = htmlentities($data->taa_datum);
         }
+        $query = "INSERT INTO taak SET taa_datum = '$taa_datum', taa_omschr = '$taa_omschr'";
+        $stmt = $this->pdo->prepare($query);
 
-        $stmt->bindParam(':taa_omschr', $taa_omschr);
-        $stmt->bindParam(':taa_datum', $taa_datum);
 
         if ($stmt->execute()) echo 'Ding! Taak aangemaakt!';
         else {
@@ -39,20 +37,14 @@ class ApiActions
 
     public function updateData($id){
 
-        $query = 'UPDATE taak SET taa_omschr = :taa_omschr, taa_datum = :taa_datum where taa_id = :taa_id';
         $data = json_decode(file_get_contents('php://input'));
-        $stmt = $this->pdo->prepare($query);
 
-        //user input opkuisen
         $taa_id = htmlentities($id);
         $taa_omschr = htmlentities($data->taa_omschr);
         $taa_datum = htmlentities($data->taa_datum);
 
-
-        $stmt->bindParam(':taa_id', $taa_id);
-        $stmt->bindParam(':taa_omschr', $taa_omschr);
-        $stmt->bindParam(':taa_datum', $taa_datum);
-
+        $query = "UPDATE taak SET taa_omschr = '$taa_omschr', taa_datum = '$taa_datum' where taa_id = '$taa_id'";
+        $stmt = $this->pdo->prepare($query);
 
         if ($stmt->execute()) echo 'Ding! Taak werd aangepast.';
         else {
@@ -63,14 +55,11 @@ class ApiActions
 
 
     public function deleteData($id){
-        $query = 'DELETE FROM taak where taa_id = :taa_id';
-        $stmt = $this->pdo->prepare($query);
-
         //clean user-input
         $taa_id = htmlentities($id);
 
-        $stmt->bindParam(':taa_id', $taa_id);
-
+        $query = "DELETE FROM taak where taa_id = '$taa_id'";
+        $stmt = $this->pdo->prepare($query);
         if ($stmt->execute()) echo 'Ding! Taak werd verwijderd.';
         else {
             echo $stmt->errorInfo();
